71b5969d9e6eec725bbe3bec2fdc37fc2dfc974d
ManuelMaisano <manumaisano@gmail.com>
2024-03-11 21:14:05 +0100

    first structure e variablejs

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 
+let name = document.getElementById('name');
+
+let km = document.getElementById('km');
+
+let age = document.getElementById('age');
+
+
+let buttonElement = document.getElementById('submit') //object
+console.log(buttonElement)
+
+
+buttonElement.addEventListener('click', function () {
+	console.log('click on button')
+
+
+let priceticket = km * (0.21);
+
 
