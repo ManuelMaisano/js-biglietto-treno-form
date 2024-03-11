f1fff243efde063f440fdc9cb1528be6a15fef80
ManuelMaisano <manumaisano@gmail.com>
2024-03-11 21:34:42 +0100

    js

--- script.js
+++ script.js
@@ -14,9 +14,19 @@ buttonElement.addEventListener('click', function () {
 	console.log('click on button')
 
 //variabili prezzo biglietto
-let priceticket = km * (0.21);
+let price = km * (0.21);
 let juniorPrice = price - (price * 20 / 100).toFixed(2);
 let seniorPrice = price - (price * 40 / 100).toFixed(2);
 
 
 
+if(age < 18){
+    price = juniorPrice.toFixed(2);
+    document.getElementById('priceticket').innerHTML = 'Il prezzo del biglietto é :' + price.toFixed(2)
+} else if (age >=65){
+    price = seniorPrice.toFixed(2);
+    document.getElementById('priceticket').innerHTML = 'Il prezzo del biglietto é :' + price.toFixed(2)
+} else {
+    price = km * 0.21;
+    document.getElementById('priceticket').innerHTML = 'Il prezzo del biglietto é :' + price.toFixed(2)
+}
